d4428ab20eda97aacbff26d452d282c5e5304ab3
Yunus <yunus7270>
2021-04-22 09:16:25 -0500

    1st commit by Yunus

---
+++ src/main/java/yunuso/CucumberProject10/App.java
@@ -0,0 +1,12 @@
+package yunuso.CucumberProject10;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Merhaba Dunya!" );
+        System.out.println( "Selam Dunya!" );
+    }
+}
